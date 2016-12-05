9dba3b1aafb4885bc4950619a9947cd2420b5c6b
Dana <dana@theironyard.com>
2016-12-05 18:07:28 -0500

    updates

--- main.js
+++ main.js
@@ -14,3 +14,49 @@ givenName = "Brett";
 givenName = givenName;
 // Q: What is `givenName` set to now?
 // A: Brett
+
+/*=========================================================
+     Part 2: Simple Math
+*/
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+// A: high - low;
+
+math = high - "5";
+// Q: What is `math` set to?
+// A: high - "5";
+
+
+// Using the `high` & `low` Variables, work through each of the 4 math operators below and log the answers to the console.
+// Write Code Below:
+
+/* =========================================================
+     Part 3: Expressions
+*/
+
+
+// Create a variable to calculate your age
+// The answer should read "NAME is XX years old"
+// The answer should not be written in a comment.
+var born = 1900;
+var today = 1900;
+
+// Answers Below:
+
+
+
+// Store some information following in variables.
+var yourName;
+var instructorName;
+
+// Update the variables above so the expression reads correctly.
+// Answers Below:var yourName = "Dana";
+// instructorName = "Bobby";
+
+
+// Final Statement
+var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
