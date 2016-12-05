e2cd3c3334ed218087c3419b30df9bc737f1b8c8
Dana <dana@theironyard.com>
2016-12-05 14:29:42 -0500

    JS Practice

--- main.js
+++ main.js
@@ -1,23 +1,16 @@
-var message = "Hi Dana!";
-console.log(message);
-console.log(message);
-console.log(message);
-console.log(message);
+/*=========================================================
+    Part 1: Variables
+*/
+var givenName;
+// Q: What value is stored in `givenName` right now?
+// A: var = givenName;
 
-var firstNumber = 50;
-var secondNumber = 100;
-var thirdNumber = 500;
-var sum = firstNumber + secondNumber + thirdNumber
-console.log(sum)
 
-var sentence = "Welcome to JavaScript.";
-var oneWord = "JavaScript is one word.";
-var learning_js = "I'm excited to learn JavaScript!";
-var that = "var is a JavaScript keyword, along with several other words.";
+givenName = "Brett";
+// Q: What is `givenName` set to now?
+// A: "Brett";
 
-var name;
-name = "Dana";
-console.log(name);
-console.log(name);
-console.log(name);
-console.log(name);
+
+givenName = givenName;
+// Q: What is `givenName` set to now?
+// A: Brett
---
+++ mainpractice.js
@@ -0,0 +1,23 @@
+var message = "Hi Dana!";
+console.log(message);
+console.log(message);
+console.log(message);
+console.log(message);
+
+var firstNumber = 50;
+var secondNumber = 100;
+var thirdNumber = 500;
+var sum = firstNumber + secondNumber + thirdNumber
+console.log(sum)
+
+var sentence = "Welcome to JavaScript.";
+var oneWord = "JavaScript is one word.";
+var learning_js = "I'm excited to learn JavaScript!";
+var that = "var is a JavaScript keyword, along with several other words.";
+
+var name;
+name = "Dana";
+console.log(name);
+console.log(name);
+console.log(name);
+console.log(name);
